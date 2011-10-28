9ec61ad695972d2d905920f9454318623808401a
Wade West <wwest81@gmail.com>
2011-10-28 09:12:30 -0500

    Added test for Nokogiri::XML::Node#appendable_node

--- spec/peppercorn/xml/node_spec.rb
+++ spec/peppercorn/xml/node_spec.rb
@@ -3,16 +3,20 @@ require 'spec_helper'
 describe String do
   
   before do
-    @string = "<p><em class=\"strong\">Hello World</em>, I <i>can</i> make HTML truncation easy.</p>"
+    @string = "<p class=\"appendable\"><em class=\"strong\">Hello World</em>, I <i>can</i> make HTML truncation <i>easy.</i></p>"
     @html = Nokogiri::HTML::DocumentFragment.parse @string
   end
 
   it "should properly truncate some simple HTML" do
-    @html.truncate(6).should eql "<p><em class=\"strong\">Hello World</em>, I <i>can</i> make HTML&#8230;</p>"
+    @html.truncate(6).should eql "<p class=\"appendable\"><em class=\"strong\">Hello World</em>, I <i>can</i> make HTML&#8230;</p>"
   end
 
   it "should not truncate some HTML that is shorter than length" do
     @html.truncate(30).should eql @string
+  end
+  
+  it "should return the last appendable node" do
+    @html.appendable_node.attribute('class').to_s.should eql "appendable"
   end
   
 end
