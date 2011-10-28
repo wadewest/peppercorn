1e66e19ad1e800d37a3265f96c474e6ea34860fb
Wade West <wwest81@gmail.com>
2011-10-28 09:12:17 -0500

    Added Nokogiri::XML::Node#appendable_node

--- lib/peppercorn/xml/node.rb
+++ lib/peppercorn/xml/node.rb
@@ -42,20 +42,26 @@ module Peppercorn
         end
         
         if !opts[:tail].to_s.empty? and overran
-          append_to = target.children.last
-          loop do
-            if append_to.text? or append_to.description.inline? # We want to append to the last block-level element
-              append_to = append_to.parent
-              break;
-            end
-            append_to = append_to.children.last
-          end
-          append_to << Nokogiri::HTML::DocumentFragment.parse(opts[:tail])
+          target.appendable_node << Nokogiri::HTML::DocumentFragment.parse(opts[:tail])
         end
         
         target = target.inner_html unless opts[:return_node]
         return opts[:return_hash] ? {:text => target, :count => [count, length].min, :overran => overran} : target
     
+      end
+      
+      # Returns the last appendable child node (the last block-level element)
+      # return [Nokogiri::XML::Node]
+      # since 0.0.2
+      def appendable_node
+        current = children.last
+        loop do
+          if current.text? or current.description.inline?
+            return current.parent
+          end
+          current = current.children.last
+        end
+        return current
       end
   
       # Returns the last child node that is a text node
